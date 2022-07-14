1348e94c11f6296c19117a7e9603f7e9c6b21bae
CAP42SIN <fastrerold@naver.com>
2022-07-14 10:37:28 +0900

    section3

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
+
 new fullpage('#fullpage', {
 	
 	autoScrolling:true,
 	scrollHorizontally: true,
     scrollBar:true,
-    sectionsColor: ['#1bbc9b', '#FFB2F5', '#FFC19E', '#BCE55C','#5CD1E5'],
+    sectionsColor: ['#fff', '#ffb2f5', '#ffc19e', '#bce55c', '#5cd1e5'],
     navigation: true,
 	navigationPosition: 'right',
     navigationTooltips: ['GAH소개','제다큐어','연구개발','본사위치','연구소위치'], 
     easing: 'easeInOutCubic',
     anchors: ['s1', 's2', 's3', 's4', 's5'],
     menu: '.gnb_list',
+    normalScrollElements: '#s2',
 });
 
